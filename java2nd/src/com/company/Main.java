1e62ed5de7088d08d67c6d3fe6ea52ac3d2382b3
raoufzanati1999 <85125013+raoufzanati1999@users.noreply.github.com>
2022-04-22 10:14:34 +0300

    Add files via upload

---
+++ java2nd/src/com/company/Main.java
@@ -0,0 +1,29 @@
+
+public class FibonacciSequence {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the index of Fibonacci Sequence");
+
+        Scanner userInput = new Scanner(System.in);
+        int n = userInput.nextInt();
+
+        int num1 = 0;
+        int num2 = 1;
+        int sum = 0;
+
+        if(n>1){
+            for(int i = 0; i < n; i++){
+
+                sum = num1 + num2;
+
+                num1 = num2;
+                num2 = sum;
+
+            }
+        }
+
+        System.out.println("\nThe fn of " + n + " is " + num1);
+
+    }
+}
